e07800615f1ca6cda8537e5d6dfaea556205707e
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-11 10:37:37 -0500

    Add sample data

---
+++ schema/scripts/20160111-103734.sql
@@ -0,0 +1,10 @@
+insert into groups(id, name, updated_by_user_id) values ('1', '   ', 'usr');
+
+
+insert into groups(id, name, updated_by_user_id) values ('1', 'Admin', 'usr');
+insert into groups(id, name, updated_by_user_id) values ('2', 'Members', 'usr');
+
+update groups set name = 'Administrators', updated_by_user_id = 'usr' where id = '1';
+
+insert into groups(id, name, updated_by_user_id) values ('3', 'Tmp', 'usr');
+update groups set deleted_at = now(), updated_by_user_id = 'hacker' where id = '3';
